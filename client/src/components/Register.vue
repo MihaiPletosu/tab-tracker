b1497c02b272890468d1a6dc3b379ec9f00d1895
pletosu.mihai@gmail.com <pletosu.mihai@gmail.com>
2019-05-06 19:46:48 +0300

    - Client side: Added Vuetify framework. - Client side: Styled Register Component.

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,23 +1,34 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input
-      type="email"
-      name="email"
-      placeholder="email"
-      v-model="email"/>
-    <br>
-    <input
-      type="password"
-      name="password"
-      placeholder="password"
-      v-model="password"/>
-    <br/>
-    <button
-      @click="register">
-      Register
-    </button>
-  </div>
+<v-layout>
+  <v-flex xs6 offset-xs3>
+    <div class="white elevation-2">
+      <v-toolbar flat dense class="cyan" dark>
+        <v-toolbar-title>Register</v-toolbar-title>
+      </v-toolbar>
+      <div class="pl-4 pr-4 pt-2 pb-2">
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          v-model="email"/>
+        <br>
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          v-model="password"/>
+        <br/>
+        <div class="error" v-html="error">
+          <br>
+        </div>
+        <v-btn class="cyan"
+          @click="register">
+          Register
+        </v-btn>
+      </div>
+    </div>
+  </v-flex>
+</v-layout>
 </template>
 
 <script>
@@ -27,18 +38,28 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     };
   },
   methods: {
     async register () {
+      try{
       const response = await AuthenticationService.register({
         email: this.email,
         password: this.password
       });
-
-      console.log(response.data);
+      }
+      catch(error){
+        this.error = error.response.data.error;
+      }
     }
   }
 };
 </script>
+
+<style scoped>
+  .error{
+    color: red;
+  }
+</style>
