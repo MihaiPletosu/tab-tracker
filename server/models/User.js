8911144f972d739036a12412db950f5f78069f2a
pletosu.mihai@gmail.com <pletosu.mihai@gmail.com>
2019-05-10 21:23:04 +0300

    [Server] Added jwt. [Server] Added password hashing.

--- server/models/User.js
+++ server/models/User.js
@@ -1,3 +1,16 @@
+const bcrypt = require('bcrypt');
+
+function hashPassword (user, options) {
+  const SALT_FACTOR = 10;
+  if (!user.changed('password')) {
+    return;
+  }
+  var salt = bcrypt.genSaltSync(SALT_FACTOR);
+  var hash = bcrypt.hashSync(user.password, salt);
+
+  return user.setDataValue('password', hash);
+}
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: {
@@ -7,7 +20,15 @@ module.exports = (sequelize, DataTypes) => {
     password: {
       type: DataTypes.STRING
     }
+  }, {
+    hooks: {
+      beforeSave: hashPassword
+    }
   });
+
+  User.prototype.comparePassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
 
   return User;
 };
