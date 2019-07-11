c724fe23ea3ef409ae679e243e20a7dbd27e5438
pletosu.mihai@gmail.com <pletosu.mihai@gmail.com>
2019-07-11 20:44:35 +0300

    View song improvements. Added tab section.

--- client/src/views/ViewSong.vue
+++ client/src/views/ViewSong.vue
@@ -1,10 +1,37 @@
 <template>
-    <panel title="Song">
-        {{song.title}}
-        {{song.artist}}
-        {{song.genre}}
-        {{song.album}}
-    </panel>
+    <v-layout>
+        <v-flex xs6>
+            <panel title="Song">
+                <v-layout row wrap>
+                    <v-flex xs6>
+                        <div class="song-title">
+                            {{song.title}}
+                        </div>
+                        <div class="song-artist">
+                            {{song.artist}}
+                        </div>
+                        <div class="song-genre">
+                            {{song.genre}}
+                        </div>
+                    </v-flex>
+
+                    <v-flex xs6>
+                        <img class="album-image" :src="song.albumImageUrl"/>
+                        <br/>
+                        {{song.album}}
+                    </v-flex>
+                </v-layout>
+            </panel>
+        </v-flex>
+        <v-flex xs6 class="ml-2">
+            <panel title="Tabs">
+                 <textarea
+                    readonly
+                    v-model="song.tab"
+                    ></textarea>
+            </panel>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
@@ -28,4 +55,18 @@ export default {
 </script>
 
 <style scoped>
+.album-image{
+    max-width: 100%;
+}
+
+textarea{
+    width: 100%;
+    font-family: monospace;
+    border: none;
+    height: 600px;
+    border-style: none;
+    border-color: transparent;
+    overflow: auto;
+    padding: 40px;
+}
 </style>
