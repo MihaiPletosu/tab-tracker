1377a951046d260a1cef4930a0ec2d58e9cf14bf
pletosu.mihai@gmail.com <pletosu.mihai@gmail.com>
2019-07-24 22:22:33 +0300

    - Added Edit server side functionality.

--- server/controllers/SongsController.js
+++ server/controllers/SongsController.js
@@ -20,7 +20,7 @@ module.exports = {
       res.send(song);
     } catch (err) {
       res.status(500).send({
-        error: 'An error occured while trying to fetch the song.'
+        error: 'An error occured while trying to show the song.'
       });
     }
   },
@@ -32,6 +32,22 @@ module.exports = {
     } catch (err) {
       res.status(500).send({
         error: 'An error occured while trying to create the song.'
+      });
+    }
+  },
+
+  async put (req, res) {
+    try {
+      const song = await Song.update(req.body, {
+        where: {
+          id: req.params.songId
+        }
+      });
+      res.send(req.body);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({
+        error: 'An error occured while trying to update this song.'
       });
     }
   }
